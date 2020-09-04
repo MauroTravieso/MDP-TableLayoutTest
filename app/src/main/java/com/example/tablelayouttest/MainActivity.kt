e4f85d2fd81dff359583a50095306759313f4679
Mauro Travieso <mauro_travieso@hotmail.com>
2020-09-04 18:00:24 -0500

    Android Kotlin - MDP TableLayoutTest

--- app/src/main/java/com/example/tablelayouttest/MainActivity.kt
+++ app/src/main/java/com/example/tablelayouttest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tablelayouttest
 
 import android.content.Context
+import android.graphics.Color.red
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,10 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
-    //val tableLayout = findViewById(R.id.table_layout_table) as TableLayout
     fun click(view : View) {
 
         context = getApplicationContext()
@@ -32,11 +31,13 @@ class MainActivity : AppCompatActivity() {
         // Add values into the row by calling addView()
         val textView1 = TextView(context)
         textView1.setText(editText1.text.toString())
-        tableRow.addView(editText1, 0)
+        textView1.setBackgroundResource(R.color.rowColor)
+        tableRow.addView(textView1, 0)
 
         val textView2 = TextView(context)
         textView2.setText(editText2.text.toString())
-        tableRow.addView(editText2,1)
+        textView2.setBackgroundResource(R.color.rowColor)
+        tableRow.addView(textView2,1)
 
         // Add the created row into the layout
         tableLayoutId.addView(tableRow)
